8bdc2b9ed22b30019bab81a843b254c103de462e
Simon Castella <castella.simon@gmail.com>
2018-06-05 11:23:21 +0200

    clean the code

--- src/main/scala/NYCTaxiApp.scala
+++ src/main/scala/NYCTaxiApp.scala
@@ -4,11 +4,13 @@ import java.util.concurrent.TimeUnit
 
 import com.esri.core.geometry.Point
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions.udf
 import spray.json._
 import utils.{Feature, FeatureCollection}
 import utils.GeoJsonProtocol._
+import org.apache.spark.sql.functions._
 
 object NYCTaxiApp {
   val spark: SparkSession = {
@@ -63,6 +65,9 @@ object NYCTaxiApp {
     )
   }
 
+
+
+  // Define some useful UDF
   val hours = (pickup: Long, dropoff: Long) => {
     TimeUnit.HOURS.convert(dropoff - pickup, TimeUnit.MILLISECONDS)
   }
@@ -78,9 +83,6 @@ object NYCTaxiApp {
     now.setTimeInMillis(pickup)
     now.get(Calendar.HOUR_OF_DAY)
   }
-
-
-  // Average trips duration in Manhattan by hour
 
 
 
@@ -101,26 +103,30 @@ object NYCTaxiApp {
     }
   }
 
-  val geojson = scala.io.Source.fromFile("data/nyc-boroughs.geojson").mkString
-  val features = geojson.parseJson.convertTo[FeatureCollection]
-  val areaSortedFeatures = features.sortBy(f => {
+
+  /*
+     Parse the nyc_boroughs.geojson and wrap is into
+   */
+  val geojson: String = scala.io.Source.fromFile("data/nyc-boroughs.geojson").mkString
+  val features: FeatureCollection = geojson.parseJson.convertTo[FeatureCollection]
+  val areaSortedFeatures: IndexedSeq[Feature] = features.sortBy(f => {
     val borough = f("boroughCode").convertTo[Int]
     (borough, -f.geometry.area2D())
   })
+  val bFeatures: Broadcast[IndexedSeq[Feature]] = sc.broadcast(areaSortedFeatures)
 
-  val bFeatures = sc.broadcast(areaSortedFeatures)
-
-  val bLookup = (x: Double, y: Double) => {
+  val bLookup: (Double, Double) => String = (x: Double, y: Double) => {
     val feature: Option[Feature] = bFeatures.value.find(f => {
       f.geometry.contains(new Point(x, y)) })
     feature.map(f => { f("borough").convertTo[String]
     }).getOrElse("NA")
   }
 
+
   /*
-  We will create a boroughDuration method that takes two instances of the Trip class and computes both the borough
-  of the first trip and the duration in seconds between the dropoff time of the first trip and the pickup time of
-  the second
+    We will create a boroughDuration method that takes two instances of the Trip class and computes both the borough
+    of the first trip and the duration in seconds between the dropoff time of the first trip and the pickup time of
+    the second
    */
   def boroughDuration(t1: Trip, t2: Trip): (String, Long) = {
     val b = bLookup(t1.dropoffX, t1.dropoffY)
@@ -128,8 +134,11 @@ object NYCTaxiApp {
     (b, d)
   }
 
-  def main(args: Array[String]): Unit = {
 
+  /*
+     import, clean and prepare the data for analysis
+   */
+  def importAndClean(): Dataset[Trip] ={
     val taxiRaw = spark.read.option("header", "true").csv("data/trip_data_1_1000k.csv")
     taxiRaw.show(10)
     val safeParse = safe(parse)
@@ -146,38 +155,57 @@ object NYCTaxiApp {
       show()
 
     // Keep only the "typical" rides.
-    val taxiClean = taxiGood.where(
-      "hours(pickupTime, dropoffTime) BETWEEN 0 AND 3"
-    )
+    val taxiClean = taxiGood.where("hours(pickupTime, dropoffTime) BETWEEN 0 AND 3")
+
+    // Drop 0 0 coordinates
+    val taxiDone = taxiClean.where("dropoffX != 0 and dropoffY != 0 and pickupX != 0 and pickupY != 0")
+
+    taxiDone.cache()
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val taxiDone = importAndClean()
 
     val boroughUDF = udf(bLookup)
+    val dayOfWeekUDF = udf(dayOfWeek)
+    val hourOfDayUDF = udf(hourOfDay)
 
-    import org.apache.spark.sql.functions._
+    // Get the average dropOff location by borough
+    taxiDone.
+      groupBy(boroughUDF($"dropoffX", $"dropoffY").as("Borough")).
+      agg(avg("dropoffX"), avg("dropoffY"), stddev("dropoffX"), stddev("dropoffY")).
+      show()
 
-    val taxiDone = taxiClean.where(
-      "dropoffX != 0 and dropoffY != 0 and pickupX != 0 and pickupY != 0"
-    ).cache()
 
-    /*taxiDone.
-      groupBy(boroughUDF($"dropoffX", $"dropoffY")).
-      count().
-      orderBy(desc("count")).
-      show()*/
+    // Group by passengers and count
+    val averageNumberOfPassenger = taxiDone.groupBy("passenger_count").count()
+    averageNumberOfPassenger.show()
 
+
+
+    // Sessionization
+    /*
+      Our goal, from many pages ago, was to investigate the relationship between the borough
+      in which a driver drops his passenger off and the amount of time it takes to
+      acquire another fare. At this point, the taxiDone data set contains all of the individual
+      trips for each taxi driver in individual records distributed across different partitions
+      of the data. To compute the length of time between the end of one ride and the start
+      of the next one, we need to aggregate all of the trips from a shift by a single driver
+      into a single record, and then sort the trips within that shift by time. The sort step
+      allows us to compare the dropoff time of one trip to the pickup time of the next trip.
+      This kind of analysis, in which we want to analyze a single entity as it executes a series
+      of events over time, is called sessionization, and is commonly performed over web
+      logs to analyze the behavior of the users of a website.
+    */
     val sessions = taxiDone. repartition($"license"). sortWithinPartitions($"license", $"pickupTime")
     sessions.cache()
 
-
-    val averageNumberOfPassenger = taxiDone.groupBy("passenger_count").count().show()
-
-    val dayOfWeekUDF = udf(dayOfWeek)
-    val hourOfDayUDF = udf(hourOfDay)
     taxiDone.select(dayOfWeekUDF($"pickupTime").as("dayOfWeek"),hourOfDayUDF($"pickupTime").as("hourOfDay"), $"passenger_count").
       groupBy("dayOfWeek", "hourOfDay").
       sum("passenger_count").
       sort("dayOfWeek", "hourOfDay").
       show(7*24)
-
 
     val boroughDurations: DataFrame = sessions.mapPartitions(trips => {
       val iter: Iterator[Seq[Trip]] = trips.sliding(2)
@@ -192,6 +220,10 @@ object NYCTaxiApp {
       sort("hours").
       show()
 
+    /*
+        How long it takes for a
+        driver to find his next fare after a dropoff in a particular borough.
+     */
     boroughDurations.
       where("seconds > 0 AND seconds < 60*60*4").
       groupBy("borough").
